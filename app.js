90841e53e6e7721f43b8f5de07add50f6679a2ea
HARIHARANATH <154905590+Hari-hara7@users.noreply.github.com>
2024-01-30 20:09:59 +0530

    deploy

---
+++ app.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const headingText = "Empower Your Learning Journey";
+    const paragraphText =
+      "Explore various learning paths, download study materials, and enhance your skills in web development, data structures and algorithms, and UI/UX design.";
+  
+    const headingElement = document.getElementById("typing-heading");
+    const paragraphElement = document.getElementById("typing-paragraph");
+  
+    function typeWriter(element, text, speed) {
+      let i = 0;
+      element.innerHTML = "";
+  
+      function type() {
+        if (i < text.length) {
+          element.innerHTML += text.charAt(i);
+          i++;
+          setTimeout(type, speed);
+        } else {
+          element.nextElementSibling.style.display = "none";
+        }
+      }
+  
+      type();
+    }
+  
+    typeWriter(headingElement, headingText, 100);
+    setTimeout(() => typeWriter(paragraphElement, paragraphText, 30), headingText.length * 100);
+  });
+  
